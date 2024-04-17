4cbe38007a28a7b280edfd7df8b2a55900fb7d85
markthitrin <thitrinwork@gmail.com>
2024-04-17 15:27:57 +0700

    put LSTM model into main

--- LSTM_demo.py
+++ LSTM_demo.py
@@ -4,30 +4,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-
 class LSTM(nn.Module):
-    def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
+    def __init__(self, input_size=1, hidden_layer_size=512, output_size=1):
         super().__init__()
         self.hidden_layer_size = hidden_layer_size
 
-        self.lstm = nn.LSTM(input_size, hidden_layer_size)
-
-        self.linear = nn.Linear(hidden_layer_size, output_size)
-
-        self.hidden_cell = (
-            torch.zeros(1, 1, self.hidden_layer_size),
-            torch.zeros(1, 1, self.hidden_layer_size),
-        )
+        self.lstm = nn.LSTM(input_size, hidden_layer_size, 2, batch_first=True, bias=False)
+        self.drop_out = nn.Dropout(p=0.2)
+        self.layer_norm = nn.LayerNorm(self.hidden_layer_size)
+        self.linear = nn.Linear(hidden_layer_size, output_size, bias=False)
 
     def forward(self, input_seq):
-        lstm_out, self.hidden_cell = self.lstm(
-            input_seq.view(len(input_seq), 1, -1), self.hidden_cell
-        )
-        predictions = self.linear(lstm_out.view(len(input_seq), -1))
-        return predictions.view(-1)  # Flatten to shape (batch_size,)
+        lstm_out, self.hidden_cell = self.lstm(input_seq)
+        lstm_norm = self.layer_norm(self.drop_out(lstm_out))
+        predictions = self.linear(lstm_norm[:,-1,:])
+        return predictions
 
     def reset_hidden_state(self):
-        self.hidden_cell = (
-            torch.zeros(1, 1, self.hidden_layer_size),
-            torch.zeros(1, 1, self.hidden_layer_size),
-        )
+        pass
